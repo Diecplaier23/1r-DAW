a9a2dd541a50a2df4812312a8e24fb58913283fb
Diecplaier23 <diegoguardia2007@gmail.com>
2026-09-24 10:44:52 +0200

    24-09 bisu

--- src/Refuerzo/Ejercicio21.java
+++ src/Refuerzo/Ejercicio21.java
@@ -4,18 +4,21 @@ import java.util.Scanner;
 
 public class Ejercicio21 {
     public static void main(String[] args) {
-        int metros, dobleces=0;
-        float micras;
+        int dobleces = 0;
+        double grosorMicras, alturaMetros;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Introduce el grosor del papel en micras:");
-        micras = sc.nextFloat();
-        System.out.println("Introduce la altura del edificio en metros:");
-        metros = sc.nextInt();
-        do{
-            dobleces++;
-            micras*=2;
-        }while (micras < metros);
-        System.out.println("El numero de veces que hay que doblar el papel para alcanzar la altura del edificio es: " + dobleces);
 
+        System.out.println("Introduce el grosor del papel en micras:");
+        grosorMicras = sc.nextDouble();
+        System.out.println("Introduce la altura del edificio en metros:");
+        alturaMetros = sc.nextDouble();
+        double grosorMetros = grosorMicras * 0.000001;
+
+        do {
+            dobleces++;
+            grosorMetros *= 2;
+        } while (grosorMetros < alturaMetros);
+
+        System.out.println("El numero de veces que hay que doblar el papel para alcanzar la altura del edificio es: " + dobleces);
     }
 }
