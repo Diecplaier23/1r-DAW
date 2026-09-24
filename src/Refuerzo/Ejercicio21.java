ca5947590eeacb44260f85baf09c90de592bc5be
Diecplaier23 <diegoguardia2007@gmail.com>
2026-09-24 10:35:22 +0200

    24-09 bis

--- src/Refuerzo/Ejercicio21.java
+++ src/Refuerzo/Ejercicio21.java
@@ -1,4 +1,21 @@
 package Refuerzo;
 
+import java.util.Scanner;
+
 public class Ejercicio21 {
+    public static void main(String[] args) {
+        int metros, dobleces=0;
+        float micras;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el grosor del papel en micras:");
+        micras = sc.nextFloat();
+        System.out.println("Introduce la altura del edificio en metros:");
+        metros = sc.nextInt();
+        do{
+            dobleces++;
+            micras*=2;
+        }while (micras < metros);
+        System.out.println("El numero de veces que hay que doblar el papel para alcanzar la altura del edificio es: " + dobleces);
+
+    }
 }
